5e86b75700467eb73adf80e20b9f6819dd332861
VictorZima <victorzima@icloud.com>
2022-08-01 04:43:21 +0300

    Initial Commit

---
+++ ToDoDemo/ToDoDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  ToDoDemoApp.swift
+//  ToDoDemo
+//
+//  Created by VictorZima on 01/08/2022.
+//
+
+import SwiftUI
+
+@main
+struct ToDoDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
